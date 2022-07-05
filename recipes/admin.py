55a4299abada36edf246e4458a71efc1c9e5b5b4
Karl Moran <karlmoran06@gmail.com>
2022-07-05 12:20:09 +0000

    Added admin post/comment feilds to display

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comments
 
-# Register your models here.
+# Register your models here.pyt
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'published_on')
+    search_fields = (['title', 'content'])
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('description', 'ingredients', ' preparation_steps')
 
 
 
 admin.site.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on')
+    search_fields = ('name', 'email', 'body')
