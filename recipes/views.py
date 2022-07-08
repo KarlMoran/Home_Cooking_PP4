f2e77af7e26a4f815bda532df90d62d4102067bb
Karl Moran <karlmoran06@gmail.com>
2022-07-08 10:17:57 +0000

    Add recipes from database to home page

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Post
+from django.views import View
+from django.db.models import Count
+
 
 # Create your views here.
 class Register(TemplateView):
@@ -14,13 +17,24 @@ class PostList(TemplateView):
     """
     Home page view
     """
-    template_name = 'index.html'
+    def get(self, request):
+        """
+        get request
+        """
+        posts = Post.objects.order_by('-published_on')[:4]
+        context = {
+            "posts": posts,
+        }
+        return render(request, 'index.html', context)
+
 
 class AllRecipes(TemplateView):
     """
     all_recipes view
     """
     template_name='all_recipes.html'
+
+
 
 class LogIn(TemplateView):
     """
